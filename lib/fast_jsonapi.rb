5e8d28def1ec969996b18668407cdddacda61789
Kevin Solkowski <ksolkowski@gmail.com>
2022-07-14 15:37:55 -0500

    well that's embarrassing

--- lib/fast_jsonapi.rb
+++ lib/fast_jsonapi.rb
@@ -6,7 +6,5 @@ module FastJsonapi
   require 'fast_jsonapi/object_serializer'
   if defined?(::Rails)
     require 'fast_jsonapi/railtie'
-  elsif defined?(::ActiveRecord)
-    require 'extensions/has_one'
   end
 end
